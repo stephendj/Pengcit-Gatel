ca39f60246a8d3f33a778bed576524c1dfe52908
Jeffrey Lingga <jeffhorus19@gmail.com>
2015-08-31 13:28:39 +0700

    remove unused libraries

--- 01 ColorCount/src/colorcount/Main.java
+++ 01 ColorCount/src/colorcount/Main.java
@@ -1,12 +1,8 @@
 package colorcount;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.io.File;
-import java.io.IOException;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
