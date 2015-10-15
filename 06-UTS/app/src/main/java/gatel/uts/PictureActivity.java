6e52b5fb10b93b45006c249a0a5e05cc58147733
stephendj <stephendj181194@gmail.com>
2015-10-15 17:51:36 +0700

    Added grayscale image, simple frequency graph

--- 06-UTS/app/src/main/java/gatel/uts/PictureActivity.java
+++ 06-UTS/app/src/main/java/gatel/uts/PictureActivity.java
@@ -3,6 +3,8 @@ package gatel.uts;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.audiofx.Equalizer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,12 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.io.FileInputStream;
 
 public class PictureActivity extends AppCompatActivity {
 
     private Bitmap bitmap;
     private ImageView imageView;
+    private ImageEqualizer equalizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +49,24 @@ public class PictureActivity extends AppCompatActivity {
             FileInputStream is = this.openFileInput(filename);
             bitmap = BitmapFactory.decodeStream(is);
             is.close();
-
-            imageView.setImageBitmap(bitmap);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
+        if(bitmap != null) {
+            equalizer = ImageEqualizer.create(bitmap);
+            imageView.setImageBitmap(equalizer.getBaseImage());
+
+            GraphView graph = (GraphView) findViewById(R.id.graph);
+            int[] frequency = equalizer.getColorFrequency();
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+
+            int increment = 0;
+            for(Integer i : frequency) {
+                series.appendData(new DataPoint(increment, i), false, 255);
+                ++increment;
+            }
+            graph.addSeries(series);
+        }
+    }
 }
